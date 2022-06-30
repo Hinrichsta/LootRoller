d75fe6e8269255116e2ac4f35be553a6d6135d80
Tyler Hinrichs <tyler.hinrichs@bosselman.com>
2022-06-30 14:27:38 -0500

    SQL Check Test

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sqlite3
 from sqlite3 import Error
 import os
 from DBCreate import *
+import random
 
 path = "C:\Projects\Python\LootRoller\loot.db"
 if not os.path.exists(path):
@@ -10,3 +11,10 @@ if not os.path.exists(path):
 con = sqlite3.connect(path)
 
 c = con.cursor()
+
+roll = random.randint(1,39)
+sql = "SELECT * FROM WeaponTypes WHERE WTypeid = " + str(roll)
+print(sql)
+gotten = c.execute(sql)
+data = c.fetchall()
+print(data)
